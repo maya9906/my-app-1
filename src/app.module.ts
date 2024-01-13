4a7af32b4256bc9e6461155275911a87e1294144
maya9906 <mayasato99@gmail.com>
2024-01-13 15:00:50 +0900

    モジュールとクラス追加まで

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql'; // ここで ApolloDriverConfig をインポート
+import { ApolloDriverConfig } from '@nestjs/graphql';
+import { join } from 'path';  
+import { PrismaService } from './prisma.service'; // PrismaService をインポートする
+import { PostsResolver } from 'src/posts/models/posts.resolver'
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, PostsResolver], // PostsResolver が提供されているか確認する
 })
 export class AppModule {}
+
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { GraphQLModule } from '@nestjs/graphql';
+// import { ApolloDriverConfig } from '@nestjs/graphql/dist/interfaces/drivers/driver.interface';
+// import { join } from 'path';  // もしくは 'path' パッケージをインポートする必要があります
+
+// @Module({
+//   imports: [
+
+//     GraphQLModule.forRoot<ApolloDriverConfig>({
+
+
+//       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+
+//     }),
+
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService, PrismaService, PostsResolver],
+// })
+// export class AppModule {}
