b2b115ad45229ce32766211519c9bcbb291604cf
maya9906 <mayasato99@gmail.com>
2024-01-13 16:38:16 +0900

    apollo driver エクスポートできない

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql'; // ここで ApolloDriverConfig をインポート
-import { ApolloDriverConfig } from '@nestjs/graphql';
-import { join } from 'path';  
-import { PrismaService } from './prisma.service'; // PrismaService をインポートする
-import { PostsResolver } from 'src/posts/models/posts.resolver'
+import { GraphQLModule, ApolloDriver } from '@nestjs/graphql'; // ApolloDriver をインポート
+import { join } from 'path';
+import { PrismaService } from './prisma.service';
+import { PostsResolver } from 'src/posts/models/posts.resolver';
+
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: { provide: 'APOLLO_DRIVER', useValue: new ApolloDriver() }, // 例: ApolloDriver のインスタンスを指定
     }),
   ],
   controllers: [AppController],
-  providers: [AppService, PrismaService, PostsResolver], // PostsResolver が提供されているか確認する
+  providers: [AppService, PostsResolver, PrismaService],
 })
 export class AppModule {}
+
+
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { GraphQLModule } from '@nestjs/graphql'; // ここで ApolloDriverConfig をインポート
+// import { ApolloDriverConfig } from '@nestjs/graphql';
+// import { join } from 'path';  
+// import { PrismaService } from './prisma.service'; // PrismaService をインポートする
+// import { PostsResolver } from 'src/posts/models/posts.resolver'
+// @Module({
+//   imports: [
+//     GraphQLModule.forRoot<ApolloDriverConfig>({
+//       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+//     }),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService, PrismaService, PostsResolver], // PostsResolver が提供されているか確認する
+// })
+// export class AppModule {}
 
 // import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
